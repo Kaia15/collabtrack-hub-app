f9db0ce6ce36de637ac0cedcc7c00deabf57a33b
Kaia15 <baotranongtran@gmail.com>
2024-01-02 16:06:07 -0500

    fix PSQL connector

---
+++ botdistrikt-intern-server/server/create-lb-tables.js
@@ -0,0 +1,9 @@
+const server = require('./server');
+const ds = server.dataSources.db;
+const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Task', 'Author'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
+
